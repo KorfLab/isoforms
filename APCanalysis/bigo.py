5677b0cad2dd182bb62bd2146e03ea3b52918f15
carl <icacedo@ucdavis.edu>
2025-07-10 17:41:16 -0700

    up

--- APCanalysis/bigo.py
+++ APCanalysis/bigo.py
@@ -12,10 +12,11 @@ parser.add_argument('data_dir', type=str, metavar='<directory>',
 	help='directory with input fasta files')
 parser.add_argument('model', type=str, metavar='<file>',
 	help='worm splice model file')
-parser.add_argument('--max_len', required=False, type=str, default=1000, metavar='<int>', 
+parser.add_argument('--max_len', required=False, type=int, 
+	default=1000, metavar='<int>', 
 	help='max length of sequence to test [%(default)i]')
-parser.add_argument('--inc', required=False, type=str, default=100, metavar='<int>', 
-	help='increment sequence length by [%(default)i]')
+parser.add_argument('--inc', required=False, type=int, default=100, 
+	metavar='<int>', help='increment sequence length by [%(default)i]')
 
 args = parser.parse_args()
 
@@ -26,6 +27,72 @@ flank = 99
 emin = 25
 imin = 35
 
+# find frequency of dinucleotides in real sequences
+genes = {}
+k2_counts = {}
+for fasta in glob.glob(f'{args.data_dir}**.fa'):
+	name, seq = next(isoform.read_fasta(fasta))
+	gid = name.split(' ')[0]
+	dons, accs = isoform.gtag_sites(seq, flank, emin)
+	for i in range(flank+emin, len(seq)-flank-emin):
+		k2 = seq[i:i+2]
+		if len(k2) == 2:
+			if k2 not in k2_counts:
+				k2_counts[k2] = 1
+			else:
+				k2_counts[k2] += 1
+	
+	#print(len(seq), len(dons), len(accs))
+	genes[gid] = {'seq_len': len(seq), 'dcounts': len(dons), 
+					'acounts': len(accs)}
+
+k2s = [item[0] for item in k2_counts.items()]
+k2_weights = [item[1] for item in k2_counts.items()]
+
+# generate random sequences with GT/AG sites
+
+with open('bigo.out', 'w') as file:
+	file.write(f'seq_len,iso_count,n_dons,n_accs\n')
+	for i in range(300, args.max_len+args.inc, args.inc):
+		ranseq = ''.join(random.choices(k2s, weights = k2_weights, 
+						k = round(i/2)))
+		rdons, raccs = isoform.gtag_sites(ranseq, flank, emin)
+		#print(len(ranseq), len(rdons), len(raccs))
+		# just use last instance of name from line 34
+		locus = Locus(name, ranseq, args.model, countonly=True)
+		file.write(f'{len(ranseq)},{locus.isocount},{len(rdons)},'
+					f'{len(raccs)}\n')
+		file.flush()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+for i in range(300, args.max_len + args.inc, args.inc):
+	name = f'{i}'
+	ranseq = ''.join(random.choices(all_k2s, weights = all_k2_counts, 
+					k = i))
+	dons, accs = isoform.gtag_sites(ranseq, flank, emin)
+	locus = Locus(name, ranseq, args.model, countonly=True)
+	print(len(ranseq), locus.isocount, len(dons), len(accs))
+'''
+'''
 big_sum = 0
 all_dons = 0
 all_accs = 0
@@ -92,7 +159,8 @@ for i in range(300, args.max_len, args.inc):
 	rafreq = round((len(raccs) / (len(ranseq)/2)), 2)
 	ran_dfreqs.append(rdfreq)
 	ran_afreqs.append(rafreq)
-	
+'''	
+
 # why doesn't simulated freqs match actual? 
 # simulation used actual freqs?
 '''

c365084423145e5c12afa539e48e6892c6aec3bb
carl <icacedo@ucdavis.edu>
2025-07-21 13:14:12 -0700

    up

--- APCanalysis/apc_analysis.py
+++ APCanalysis/apc_analysis.py
@@ -1,11 +1,16 @@
 class PWM:
 
-	def __init__(self, fasta, gff):
+	def __init__(self, fasta, gff, don_len, don_left, acc_len, 
+					acc_right):
 		self.fasta = fasta
 		self.gff = gff
 		self.seq = None
+		self.DN = don_len
+		self.DL = don_left
+		self.AN = acc_len
+		self.AR = acc_right
 		
-	def intron_seq(self):
+	def _intron_seq(self):
 		
 		self.seq = ''
 		with open(self.fasta, 'rt') as fp:
@@ -17,7 +22,21 @@ class PWM:
 						
 		return self.seq
 		
-	def get_ss(self, seq, gff):
+	def splice_sites(self):
 		
-		print(wow)
-	
+		annotated_splice_sites = []
+		with open(self.gff, 'rt') as fp:
+			splice_site_set = {}
+			for line in fp:
+				line = line.rstrip()
+				line = line.split('\t')
+			
+				if line[6] == '-': continue
+			
+				if line[1] == 'WormBase' and line[2] == 'intron':
+					acceptor = self.seq[int(line[4])-self.AN:int(line[4])+self.AR]
+					annotated_splice_sites.append((acceptor))
+			
+			return annotated_splice_sites
+				
+				

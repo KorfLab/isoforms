cc25bca64abf351d40c0effa10dd68c3d1a7fc9d
carl <icacedo@ucdavis.edu>
2025-10-11 15:55:32 -0700

    up

--- APCanalysis/make_svg.py
+++ APCanalysis/make_svg.py
@@ -4,30 +4,59 @@ parser = argparse.ArgumentParser(
 	description='create svg file from APC isoforms')
 parser.add_argument('apc_gff')
 parser.add_argument('wb_gff')
+parser.add_argument('--limit', type=int, required=False, default=10)
 
 arg = parser.parse_args()
 
-
-c = 0 
 isoforms = {}
 with open(arg.apc_gff, 'r') as fp:
 	for line in fp:
 		line = line.rstrip()
-		if line.startswith('#'): continue
 		line = line.split('\t')
 		if len(line) != 9: continue
-		if line[2] == 'mRNA': continue
+		if line[2] == 'mRNA' or line[2] == 'gene': continue
+		feature = [line[2], int(line[3]), int(line[4])]
 		iso_parent = line[8].split('=')[1]
-		print(iso_parent)
-		print(line)
-		isoforms[iso_parent] = []
-		break
+		if iso_parent not in isoforms:
+			isoforms[iso_parent] = [feature]
+		else:
+			isoforms[iso_parent].append(feature)		
 
-		c += 1
-		if c == 20: break
-		
-print('#########')		
+sorted_isos = []
+for item in isoforms.items():
+	sorted_iso = sorted(item[1], key=lambda index : index[2])
+	sorted_isos.append(sorted_iso)
+	
+def draw_rect(width, height, x, y, fill):
+	
+	rect = f'<rect width="{width}" height="{height}" x="{x}" y="{y}" fill="{fill}" />\n'
+	
+	return rect
+	
+with open('isoforms.svg', 'w') as fp:
+	fp.write(f'<svg width="1000" height="1000">\n')
+	y = 0
+	for iso in sorted_isos[:arg.limit]:
+		print(iso)
+		for exin in iso:
+			if exin[0] == 'exon':
+				height = 20
+				width = exin[2] - exin[1] + 1
+				rect = draw_rect(width, height, exin[1], y, 'blue')
+				fp.write(rect)
+			if exin[0] == 'intron':
+				height = 6
+				width = exin[2] - exin[1] + 1
+				rect = draw_rect(width, height, exin[1], y+7, 'pink')
+				fp.write(rect)
+		y += 30
+	fp.write(f'</svg>')
+	
+	
 
+	
+
+'''
 rna_introns = {}
 with open(arg.wb_gff, 'r') as fp:
 	for line in fp:
@@ -47,3 +76,4 @@ with open('isoforms.svg', 'w') as fp:
 	fp.write(f'<rect width="300" height="100" x="110" y="220" '
 			'style="fill:rgb(0,0,255);stroke-width:3;stroke:red" />\n')
 	fp.write(f'</svg>\n')
+'''

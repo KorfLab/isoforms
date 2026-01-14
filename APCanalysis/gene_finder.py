1712855727286d221c450b894d8cecd026865ae1
carl <icacedo@ucdavis.edu>
2026-01-14 11:00:28 -0800

    up

--- APCanalysis/gene_finder.py
+++ APCanalysis/gene_finder.py
@@ -6,34 +6,49 @@ parser = argparse.ArgumentParser(
 parser.add_argument('genome', help='')
 parser.add_argument('coordinates', help='chromosome:loc i.e. X:99:999; ' 
 	'I, II, III, IV, V, X, MtDNA')
+parser.add_argument('seq_desc', help='add description for '
+	'sequence in first line of fasta and file name')
 
 args = parser.parse_args()
 
-# dyn-1 X:15568921:15573031
+# dyn-1 whole gene sequence is X:15568921:15573021
+# for APC, only use 15571571 to 15573021
+# includes only last 3 exons
 
-# put chromosome sequences into dictionary
-# storing in dictionary is slow
-# thow away unneeded text instead
 read_arg = args.coordinates.split(':')
-chrom = read_arg[0]
-coors = [read_arg[1], read_arg[2]]
+selected_chrom = read_arg[0]
+gen_coors = [int(read_arg[1]), int(read_arg[2])]
 
-print(chrom, coors)
+#print(selected_chrom, gen_coors)
 
 open_type = gzip.open if args.genome.endswith('.gz') else open
 with open_type(args.genome, 'rt') as fp:
-	count = 0
+	current_chrom = None
+	gen_seq = []
+	n_counts = 0
 	for line in fp:
 		line = line.rstrip()
+		
+		# check if loop is currently on the chromosome of interest
 		if line.startswith('>'):
-			if chrom == line.split('>')[1]:
-				print(line)
-				for n in line:
-					count += 1
+			current_chrom = line.split('>')[1]
+			continue
+		if current_chrom != selected_chrom: continue
+		#print(current_chrom, line)
+		
+		# build seq string
+		for n in line:
+			n_counts += 1
+			if n_counts >= gen_coors[0] and n_counts <= gen_coors[1]:
+				#print(n_counts, n)
+				gen_seq.append(n)
+			
 				
-#print(chr_seqs)
-#coors = args.coordinates.split(':')
-
-# use 1 based indexing for coordinates
-
-#print(chr_seqs[coors[0]][int(coors[1])-1:int(coors[2])])
+print(''.join(gen_seq))
+							
+if args.seq_desc:
+	seq_desc = args.seq_desc)
+else:
+	print('wow')
+	
+with open(''

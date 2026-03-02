b95f93e6eef504a0125894b0d46a150d72916747
carl <icacedo@ucdavis.edu>
2026-03-01 23:10:32 -0800

    up

--- APCanalysis/difalt/gff_search.py
+++ APCanalysis/difalt/gff_search.py
@@ -8,19 +8,21 @@ parser = argparse.ArgumentParser(description='find genes of interest '
 			'from haman genes')
 parser.add_argument('WBGenes', help='text with list of WBGene IDs and '
 			'region of interest i.e. '
-			'mod1,WBGene00003386,V,8910090,8910840,-')
+			'WBGene, gene name, chr, left bound, right bound, '
+			'first exon start, sense '
+			'WBGene00003386,mod1,V,8910090,8910840,8913992,-')
 parser.add_argument('gene_dir', help='directory with gff and fa files')
 parser.add_argument('new_dir', help='name of directory to store '
 			'fa and gff files')
 
 args = parser.parse_args()
 
-gfiles = {}
+ginfo = {}
 with open(args.WBGenes, 'rt') as fp:
 	for line in fp:
 		line = line.rstrip()
-		line = line.split(':')
-		gfiles[line[1]] = [line[0]]
+		line = line.split(',')
+		ginfo[line[0]] = line[1:]
 
 for gff_file in glob.glob(args.gene_dir + '*.gff3'):
 	with open(gff_file, 'rt') as fp:
@@ -28,15 +30,67 @@ for gff_file in glob.glob(args.gene_dir + '*.gff3'):
 			line = line.rstrip().split('\t')
 			if line[1] == 'WormBase' and line[2] == 'gene':
 				wbg = line[8].split(':')[1]
-				if wbg in gfiles:
-					gfiles[wbg].append(gff_file)
+				if wbg in ginfo:
+					ginfo[wbg].append(gff_file)
 					base = gff_file.split('/')[-1].split('.')[:-1]
-					gfiles[wbg].append(f'{args.gene_dir}'
+					ginfo[wbg].append(f'{args.gene_dir}'
 										f'{'.'.join(base)}.fa')
-
+print(ginfo)
 if not os.path.isdir(f'{args.new_dir}'):
 	os.mkdir(f'{args.new_dir}/')
+	
+# i don't think this method will work with optiso
+# need to rework make_svg instead
+coors1 = [15, 25, 30, '-']
+coors2 = [15, 25, 10, '+']
+flank = 5
+if coors1[3] == '-':
+	coors1 = [abs(coors1[1]-coors1[2])+flank+1, abs(coors1[0]-coors1[2])+flank+1]
+if coors2[3] == '+':
+	coors2 = [coors2[0]-coors2[2]+flank+1, coors2[1]-coors2[2]+flank+1]
 
+keep_lines = []
+flank = 100
+for item in ginfo.items():
+	if len(item[1]) == 1: continue
+	if item[1][5] == '-':
+		start = abs(int(item[1][3]) - int(item[1][4])) + flank+1
+		end = abs(int(item[1][2]) - int(item[1][4])) + flank+1
+		gen_coors = [start, end]
+	else:
+		start = item[1][2] - item[1][4] + flank+1
+		end = item[1][3] - item[1][4] + flank+1
+		gen_coors = [start, end]
+	with open(item[1][6], 'rt') as fp:
+		for line in fp:
+			line = line.rstrip()
+			line = line.split('\t')
+			if len(line) == 9:
+				if (line[1] == 'WormBase' and line[2] 
+					in ['exon', 'gene', 'mRNA', 'CDS', 'intron']):
+						if line[2] == 'gene': print(line)
+						# get any regions that overlap
+						if ((int(line[3]) <= gen_coors[1] and 
+							int(line[3]) >= gen_coors[0]) or 
+							(int(line[4]) <= gen_coors[1] and
+							int(line[4]) >= gen_coors[0])):
+								# negative CDS coors needed for make_svg.py
+								line[3] = str(int(line[3]) - gen_coors[0] +1)
+								line[4] = str(int(line[4]) - gen_coors[0] +1)
+								print('\t'.join(line))
+				if line[1] == 'RNASeq_splice':
+					# only get introns within coors
+					if (int(line[3]) <= gen_coors[1] and 
+						int(line[3]) >= gen_coors[0] and
+						int(line[4]) <= gen_coors[1] and
+						int(line[4]) >= gen_coors[0]):
+							line[3] = str(int(line[3]) - gen_coors[0] +1)
+							line[4] = str(int(line[4]) - gen_coors[0] +1)
+							print('\t'.join(line))
+
+
+
+'''
 for item in gfiles.items():
 	if len(item[1]) == 1: continue
 	src1 = item[1][1]
@@ -44,9 +98,7 @@ for item in gfiles.items():
 	dest = args.new_dir
 	shutil.copy(src1, dest)
 	shutil.copy(src2, dest)
-
-
-
+'''
 
 # old code to search gff
 # now using haman gene builds

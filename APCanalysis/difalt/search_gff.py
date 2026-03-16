0e00f9525a17c6c5d3c83fe2ca9b72a72feed168
carl <icacedo@ucdavis.edu>
2026-03-16 16:42:07 -0700

    up

--- APCanalysis/difalt/search_gff.py
+++ APCanalysis/difalt/search_gff.py
@@ -17,45 +17,53 @@ parser.add_argument('out_dir', help='name of directory to store '
 
 args = parser.parse_args()
 
-ginfo = {}
+gene_info = {}
 with open(args.WBGenes, 'rt') as fp:
 	for line in fp:
 		line = line.rstrip()
 		if line.startswith('#'): continue
 		line = line.split(',')
-		ginfo[line[0]] = line[1:]
-		
-print(ginfo)
+		gene_info[line[0]] = line[1:]
 
 open_type = gzip.open if args.annotation.endswith('.gz') else open
+# ('WBGene00001130', ['dyn-1', 'X', '15572271', '15573021', '15568921', '+'])
 
-count = 0
+gene_lines = {}
 with open_type(args.annotation, 'rt') as fp:
 	for line in fp:
 		line = line.rstrip()
 		line = line.split('\t')
-		#if len(line) == 9:
-			#print(line)
-			'''
-			if (line[0] == chrom and line[1] == 'WormBase' and
-				line[2] == 'CDS'):
+		for info in gene_info.items():
+			gid_info = (info[0], info[1][0])
+			if gid_info not in gene_lines:
+				gene_lines[gid_info] = []
+			if len(line) == 9:
+				f_start, f_end = int(line[3]), int(line[4])
+				g_start, g_end = int(info[1][2]), int(info[1][3])
+				
+				# match WBGene
+				if line[2] == 'mRNA':
+					wbg_gff = line[8].split(';')[1].split(':')[1]
+					if info[0] == wbg_gff:
+						# only add first entry of WBGene
+						if len(gene_lines[gid_info]) == 0:
+							gene_lines[gid_info].append(line)
+						
 				# get any CDS regions that overlap
-				if ((int(line[3]) <= gen_coors[1] and 
-					int(line[3]) >= gen_coors[0]) or 
-					(int(line[4]) <= gen_coors[1] and
-					int(line[4]) >= gen_coors[0])):
-						# negative CDS coors needed for make_svg.py
-						line[3] = str(int(line[3]) - gen_coors[0] +1)
-						line[4] = str(int(line[4]) - gen_coors[0] +1)
-						print('\t'.join(line))
-			if line[0] == chrom and line[1] == 'RNASeq_splice':
-				# only get introns within coors
-				if (int(line[3]) <= gen_coors[1] and 
-					int(line[3]) >= gen_coors[0] and
-					int(line[4]) <= gen_coors[1] and
-					int(line[4]) >= gen_coors[0]):
-						line[3] = str(int(line[3]) - gen_coors[0] +1)
-						line[4] = str(int(line[4]) - gen_coors[0] +1)
-						print('\t'.join(line))
-			'''
+				if (line[0] == info[1][1] and line[1] == 'WormBase' and
+					line[2] == 'CDS'):
+					if f_start <= g_end and f_end >= g_start:
+						gene_lines[gid_info].append(line)
+				
+				# get any introns only within 
+				if line[0] == info[1][1] and line[1] == 'RNASeq_splice': 
+					if f_start >= g_start and f_end <= g_end:		
+						gene_lines[gid_info].append(line)
+						
+
+for item in gene_lines.items():
+	print(item[0])
+						
+			
+
 

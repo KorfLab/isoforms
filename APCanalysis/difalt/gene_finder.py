357902cbc5c7e7cb54502921340ff376634898e7
carl <icacedo@ucdavis.edu>
2026-04-28 21:54:38 -0700

    up

--- APCanalysis/difalt/gene_finder.py
+++ APCanalysis/difalt/gene_finder.py
@@ -8,6 +8,8 @@ parser = argparse.ArgumentParser(
 parser.add_argument('genome', help='genomic fasta file')
 parser.add_argument('gffs', help='directory of gffs with genes of '
 	'interest, output from search_gff.py')
+parser.add_argument('out_dir', help='name of directory to store '
+			'fa and gff files')
 	
 '''
 parser.add_argument('WBGenes', help='csv with list of WBGene IDs and '
@@ -60,9 +62,6 @@ for file in glob.glob(f'{args.gffs}*'):
 
 	g_info = f'>{gene_name} {chrom}:{start}-{end} {sense} {wbg_gene}'
 	genes[g_info] = []
-
-for gene in genes.items():
-	print(gene)
 	
 open_type = gzip.open if args.genome.endswith('.gz') else open
 
@@ -84,18 +83,69 @@ with open_type(args.genome, 'rt') as fp:
 		for gene in genes.items():
 			chrom = gene[0].split(' ')[1].split(':')[0]
 			if chrom != current_chrom: continue
-			n_counts = {}
+			#n_counts = {}
 			coors = gene[0].split(' ')[1].split(':')[1].split('-')
 			coors = [int(coors[0]), int(coors[1])]
 			for n in line:
 				if gene[0] not in n_counts:
 					n_counts[gene[0]] = 0
 				n_counts[gene[0]] += 1
-				print(n_counts)
-				if coors[0] <= n_counts[gene[0]] <= end:
-					gen_seqs[item[0]].append(n)
+				if coors[0] <= n_counts[gene[0]] <= coors[1]:
+					if gene[0] not in gen_seqs:
+						gen_seqs[gene[0]] = []
+					gen_seqs[gene[0]].append(n)
+		print(gen_seqs)
 
-print(gen_seqs)
+
+# flip - strands
+for info_seq in gen_seqs.items():
+	if info_seq[0].split(' ')[2] == '-':
+		comp_seq = []
+		comps = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+		for n in info_seq[1]:
+			comp_seq.append(comps[n])
+		rev_seq = []
+		for i in range(len(comp_seq)):
+			i = i+1
+			rev_seq.append(comp_seq[-i])
+		gen_seqs[info_seq[0]] = rev_seq
+
+# organize sequences into 80 nt lines
+gen_seqs_80 = {}
+for gen_seq in gen_seqs.items():
+	#####################################print(len(gen_seq[1]))
+	seq_line = []
+	seq_lines = []
+	for n in gen_seq[1]:
+		if len(seq_line) < 80:
+			seq_line.append(n)
+		else:
+			seq_lines.append(''.join(seq_line))
+			seq_line = []
+			seq_line.append(n)
+	if len(seq_line) != 0:
+		seq_lines.append(''.join(seq_line))
+
+	gen_seqs_80[gen_seq[0]] = seq_lines
+
+if args.out_dir.endswith('/'): 
+	out = args.out_dir
+else:
+	out = f'{args.out_dir}/'
+	
+if not os.path.exists(out):
+	os.makedirs(out)
+	
+for items in gen_seqs_80.items():
+	seq_desc = (
+		f'>{items[0][1]} {items[0][2]}:{items[0][3]}-{items[0][4]} '
+		f'{items[0][7]} Gene:{items[0][0]} Gene_Start={items[0][5]} '
+		f'Gene_End={items[0][6]}'
+	)
+	with open(f'{out}{items[0][1]}.fa', 'wt') as fp:
+		fp.write(f'{seq_desc}\n')
+		for seq in items[1]:
+			fp.write(f'{seq}\n')
 
 '''
 		# match line index to region of interest 

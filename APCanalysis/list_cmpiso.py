32f9d1e291380409c2e0d0a37e0367a9eadd9d50
carl <icacedo@ucdavis.edu>
2026-06-09 00:11:57 -0700

    up

--- APCanalysis/list_cmpiso.py
+++ APCanalysis/list_cmpiso.py
@@ -6,17 +6,22 @@ import isoform
 parser = argparse.ArgumentParser(description=
 	'run cmpiso on all APC genes and organize results')
 parser.add_argument('APCisos', nargs='+', type=str, metavar='<file>', 
-	help='list of directories with APC iso results')
+	help='list of directories with APC iso results, ' 
+	'input individual directory paths')
 
 args = parser.parse_args()
 
+paths2gffs = {}
 for dpath in args.APCisos:
 
 	if dpath.endswith('/'): dpath = dpath
 	else: dpath = f'{dpath}/'
 	
-	print(dpath)
+	dir_name = dpath.split('/')[-2]
+	paths2gffs[dir_name] = []
 	for fpath in glob.glob(f'{dpath}*'):
-		print(fpath)
-
+		paths2gffs[dir_name].append(fpath)
+		
+for item in paths2gffs.items():
+	print(item)
 

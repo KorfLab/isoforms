1bac1196467f0b81765f107010a99f36907e0e89
carl <icacedo@ucdavis.edu>
2026-09-01 00:30:19 -0700

    up

--- APCanalysis/go/get_symbols.py
+++ APCanalysis/go/get_symbols.py
@@ -14,6 +14,7 @@ for gff in glob.glob(f'{args.smallgenes}*gff3'):
 		for line in fp:
 			line = line.rstrip()
 			line = line.split('\t')
+			if len(line) != 9: continue
 			if line[2] == 'gene':
 				wbgene = line[8].split(':')[1]
 				wbgenes.append(wbgene)
@@ -24,11 +25,22 @@ with open(args.annotation, 'rt') as fp:
 	for line in fp:
 		line = line.rstrip()
 		line = line.split()
-		if line[2] == 'gene':
+		if len(line) != 9: continue
+		if line[1] == 'WormBase' and line[2] == 'gene':
 			info = {x.split('=')[0]: x.split('=')[1] for x in line[8].split(';')}
 			wbg = info['Name']
 			sym = info['sequence_name']
 			symbols[wbg] = sym
-			
-print(symbols)
+
+sg_syms = {}
+not_in = []
+for g in wbgenes:
+	if g in symbols:
+		sg_syms[g] = symbols[g]
+	else:
+		not_in.append(g)
+		
+for item in sg_syms.items():
+	print(f'{item[0]},{item[1]}')
+
 

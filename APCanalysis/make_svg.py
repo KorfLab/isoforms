3f26b2237439b57f4833806891482a7efa9317bb
carl <icacedo@ucdavis.edu>
2026-09-10 14:45:03 -0700

    up

--- APCanalysis/make_svg.py
+++ APCanalysis/make_svg.py
@@ -219,7 +219,7 @@ with open(arg.out_name, 'w') as onfp:
 		height = 6
 		width = intron[0][1] - intron[0][0] + 1
 		rect = draw_rect(width, height, intron[0][0]+x_offset, y, 'green')
-		text1 = draw_text(score, start_pt[0][0]-50+x_offset, y+7)
+		text1 = draw_text(score, start_pt[0][0]-60+x_offset, y+7)
 		text2 = draw_text(f'{intron[0][0]},{intron[0][1]}', 
 							intron[0][1]+10+x_offset, y+7)
 		onfp.write(rect)
@@ -264,7 +264,7 @@ with open(arg.out_name, 'w') as onfp:
 					onfp.write(gc_text)
 				
 		int_text = '|'.join([f'{x[0]},{x[1]}' for x in int_def])
-		text1 = draw_text(prob, iso[0][1]+x_offset-55, y+15)
+		text1 = draw_text(prob, iso[0][1]+x_offset-75, y+15)
 		text2 = draw_text(int_text, iso[-1][2]+x_offset+10, y+15)
 		onfp.write(text1)
 		onfp.write(text2)
